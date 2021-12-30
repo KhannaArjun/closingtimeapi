0815706bdf980b088409a76ee4151594b9be0d28
srikamalteja <srikamalteja@gmail.com>
2021-12-30 17:49:01 +0000

    Added finding nearest recipient logic in /addFood

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 import firebase_admin
 from firebase_admin import credentials, messaging
+from math import radians, cos, sin, asin, sqrt
 
 cred = credentials.Certificate("closingtime-e1fe0-firebase-adminsdk-1zdrb-228c74a754.json")
 firebase_admin.initialize_app(cred)
@@ -245,24 +246,46 @@ def update_profile():
 def add_food():
     input = request.get_json()
     add_food_col = getCollectionName('add_food')
+    recipient_registration_col = getCollectionName('recipient_registration')
 
-    obj = add_food_col.insert_one(input)
+    food_id = add_food_col.insert_one(input)
 
-    user_firebase_token = getCollectionName("user_firebase_token")
+    recipients_obj = recipient_registration_col.find({})
 
-    objj = user_firebase_token.find({"role": constants.Utils.recipient})
-
-    print(str(objj))
-
-    l = list(objj)
+    recipients_obj_list = list(recipients_obj)
 
     ids = list()
+    # food_donations_nearby_recipients_obj_list = list()
 
-    for x in l:
-        print(x['firebase_token'])
-        ids.append(str(x['firebase_token']))
+    for item in recipients_obj_list:
+        miles = dist(float(input['pick_up_lat']), float(input['pick_up_lng']), float(item['lat']), float(item['lng']))
+        # print(miles)
+        if miles < constants.Utils.miles:
+            # print(item)
+            ids.append(str(ObjectId(item['_id'])))
+            # food_donations_nearby_recipients_obj = {
+            #     "recipient_id" : str(ObjectId(item['_id'])),
+            #     "food_id": [food_id]
+            # }
+            #
+            # food_donations_nearby_recipients_obj_list.append(food_donations_nearby_recipients_obj)
 
-    send_notifications_to_recipients(ids, input['food_name'], input['quantity'])
+    print(ids)
+
+    # food_donations_nearby_recipients_col = getCollectionName("food_donations_nearby_recipients")
+
+    user_firebase_token_col = getCollectionName("user_firebase_token")
+    recipients_firebase_tokens = user_firebase_token_col.find({"user_id": {"$in": ids}})
+
+    tokens = list()
+
+    for item in recipients_firebase_tokens:
+        print(item)
+
+        print(item['firebase_token'])
+        tokens.append(item['firebase_token'])
+
+    send_notifications_to_recipients(tokens, input['food_name'], input['quantity'])
 
     return flask.jsonify(api_response.apiResponse(constants.Utils.inserted, False, {}))
 
@@ -414,6 +437,29 @@ def get_recipient_user_profile():
     return flask.jsonify(api_response.apiResponse(constants.Utils.success, False, data))
 
 
+@app.route('/recipient/getAvailableFoodList', methods=['POST'])
+def getAvailableFoodList():
+    input = request.get_json()
+    add_food_col = getCollectionName('add_food')
+
+    data = add_food_col.find({'isFoodAccepted': input['isFoodAccepted']})
+
+    foodList = []
+    array = list(data)
+    if len(array):
+        for x in array:
+            obj = dict(x)
+            obj.update({'id': str(obj['_id'])})
+            del obj['_id']
+            miles = dist(input['recipient_lat'], input['recipient_lng'], obj['pick_up_lat'], obj['pick_up_lng'])
+
+            if miles < constants.Utils.miles:
+                foodList.append(obj)
+        array.clear()
+
+    return flask.jsonify(api_response.apiResponse(constants.Utils.success, False, foodList))
+
+
 @app.route('/recipient/accept_food', methods=['POST'])
 def accept_food():
     input = request.get_json()
@@ -483,7 +529,6 @@ def send_notifications_to_recipients(ids, food_name, quantity):
 
 
 def send_notification_to_donor(token, recipient_name):
-
     notification = messaging.Notification(title="Food Accepted by " + str(recipient_name), body="")
 
     # See documentation on defining a message payload.
@@ -492,6 +537,23 @@ def send_notification_to_donor(token, recipient_name):
         token=token,
     )
     response = messaging.send(message)
+
+
+def dist(lat1, long1, lat2, long2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lat1, long1, lat2, long2 = map(radians, [lat1, long1, lat2, long2])
+    # haversine formula
+    dlon = long2 - long1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    # Radius of earth in kilometers is 6371, use 3956 for miles
+    miles = 3956 * c
+    return miles
 
 
 def _get_access_token():
--- utils/constants.py
+++ utils/constants.py
@@ -14,6 +14,7 @@ class Utils:
     recipient = "Recipient"
     volunteer = "Volunteer"
     donor = "Donor"
+    miles = 10
 
 # def parse():
 #     json = [
