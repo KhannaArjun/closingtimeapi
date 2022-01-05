71f4c1451ad65a371f572a5ba212dc570b01a4cf
srikamalteja <srikamalteja@gmail.com>
2022-01-05 16:41:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -453,7 +453,7 @@ def getAvailableFoodList():
         for x in array:
             obj = dict(x)
             pick_up_date = datetime.strptime(obj['pick_up_date'], "%Y-%m-%d").date()
-            if pick_up_date <= present_date:
+            if pick_up_date >= present_date:
                 obj.update({"status": constants.Utils.expired})
                 obj.update({'id': str(obj['_id'])})
                 del obj['_id']
