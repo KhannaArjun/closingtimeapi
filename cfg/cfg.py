f7c33d5ef30ebd608575ebdd9e2141ee9cb710fc
srikamalteja <srikamalteja@gmail.com>
2022-03-12 23:42:37 +0000

    Update cfg.py

--- cfg/cfg.py
+++ cfg/cfg.py
@@ -8,10 +8,14 @@ def get_dev_db():
 
 
 def get_prod_db():
-    CONNECTION_STRING = "mongodb+srv://closingtimeprod:closingtimeprod@closingtime.1bd7w.mongodb.net/closingtimeprod?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+    CONNECTION_STRING = "mongodb+srv://closingtimeprod:closingtimeprod@closingtimeprod.ryu6d.mongodb.net/closingtimeprod?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
     db_name = "closingtimeprod"
     # u_name = "closingtimeprod"
     # password = "closingtimeprod"
+
+    # mongodb + srv: // < username >: < password >@closingtimeprod.ryu6d.mongodb.net / myFirstDatabase
+    #
+    # ?retryWrites = true & w = majority
 
     return CONNECTION_STRING, db_name
 
